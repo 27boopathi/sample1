8ad3babbbe5b80f8d335d537e4304dee6e2a173c
rajesh <rajeshr@megam.io>
2019-10-18 05:44:15 +0530

    intial commit

---
+++ day1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Propfirst from './component/PFirst';
+
+function App() {
+  return (
+    <div className="App">
+<Propfirst name="BOOPATHI"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day1/src/component/PFirst.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Propfirst=props=>{
+  console.log(props);
+  return(
+    <div>
+<h1>WELCOME TO {props.name}</h1>    </div>
+  )
+}
+export default Propfirst
